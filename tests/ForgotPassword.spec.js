9a5b8d4820a38652d3b2e73e7b7a6378cd9ce113
nsv <volsci@tuta.io>
2019-03-09 20:54:37 +1000

    Write Enzyme Tests for Forgot Password

---
+++ tests/ForgotPassword.spec.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import ForgotPassword from "../src/components/ForgotPassword";
+import TextField from "@material-ui/core/TextField/TextField";
+import Button from '@material-ui/core/Button';
+
+describe('SignUp', () => {
+  it('renders', () => {
+    const wrapper = shallow(<ForgotPassword />);
+
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('matches snapshot', () => {
+    const wrapper = shallow(<ForgotPassword />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('shows the email field', () => {
+    const wrapper = mount(<ForgotPassword />);
+
+    expect(wrapper.find(TextField).length).toEqual(1);
+  });
+
+  it('shows the send reset link and back buttons', () => {
+    const wrapper = mount(<ForgotPassword />);
+
+    expect(wrapper.find(Button).length).toEqual(2);
+  });
+});
