be0d66bbc551934565ed6268d78f5cbcb8d1170b
nsv <volsci@tuta.io>
2019-03-10 14:13:32 +1000

    Create Dashboard Bar

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,6 +2,28 @@ import React from 'react';
 import PropTypes, { instanceOf } from "prop-types";
 import { Cookies, withCookies } from "react-cookie";
 import { Redirect } from 'react-router-dom';
+import { withStyles } from "@material-ui/core";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
+});
 
 class Dashboard extends React.Component {
   state = {
@@ -23,6 +45,8 @@ class Dashboard extends React.Component {
   }
 
   render() {
+    const { classes } = this.props;
+
     /**
      * If a session ID was not detected, the user is sent to the login page.
      */
@@ -31,15 +55,32 @@ class Dashboard extends React.Component {
     }
 
     return (
-      <h2>Dashboard!</h2>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              Adnat
+            </Typography>
+            <IconButton
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </div>
     )
   }
 }
 
 Dashboard.propTypes = {
-  //classes: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  classes: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
   cookies: instanceOf(Cookies).isRequired,
 };
 
+Dashboard = withStyles(styles)(Dashboard); // eslint-disable-line no-class-assign
 export default withCookies(Dashboard);
 
